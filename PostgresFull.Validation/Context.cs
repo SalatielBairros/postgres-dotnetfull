419f48660409a57e962d09c19bc9726122ab3160
Salatiel Bairros <salatiel.costabairros@gmail.com>
2022-06-17 12:03:58 -0300

    initial commit

---
+++ PostgresFull.Validation/Context.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PostgresFull.Validation.Model;
+
+namespace PostgresFull.Validation
+{
+    public class Context : DbContext
+    {
+        public Context()
+        : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Person> Person { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>().ToTable("person", "public");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ PostgresFull.Validation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostgresFull.Validation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new Context())
+            {
+                var ret = context.Person.Select(x => x).ToList();
+                Console.WriteLine(ret.FirstOrDefault()?.name);
+            }
+        }
+    }
+}
